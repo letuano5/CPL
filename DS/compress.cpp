20f78074aec23444044c1f5503813450e4c2ee08
letuano5 <73065691+letuano5@users.noreply.github.com>
2022-05-08 09:48:04 +0700

    Create compress.cpp

---
+++ DS/compress.cpp
@@ -0,0 +1,18 @@
+#define all(x) (x).begin(), (x).end()
+template <class T>
+struct Compressor {
+  vector <T> val;
+
+  void add (int x) {
+    val.push_back(x);
+  }
+
+  void init() {
+    sort(all(val));
+    val.resize(unique(all(val)) - val.begin());
+  }
+
+  int idx(int x) {
+    return lower_bound(all(val), x) - val.begin() + 1;
+  }
+};
