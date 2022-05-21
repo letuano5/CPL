99652ccef50fdf0fd084279ee985d56edf3ecdaa
letuano5 <73065691+letuano5@users.noreply.github.com>
2022-05-21 22:19:55 +0700

    Update combi.cpp

--- Math/combi.cpp
+++ Math/combi.cpp
@@ -1,3 +1,24 @@
+// small n, k any MOD
+
+const int MOD = 1e9 + 9;
+const int N = 105;
+
+int comb[N][N];
+
+void prep_comb() {
+  comb[0][0] = 1;
+  for (int i = 1; i < N; i++) {
+    comb[i][0] = 1;
+    for (int j = 1; j <= i; j++) {
+      comb[i][j] = (comb[i - 1][j] + comb[i - 1][j - 1]);
+      if (comb[i][j] >= MOD)
+        comb[i][j] -= MOD;
+    }
+  }
+}
+
+// n, k <= 10^6
+
 const int MOD = 1e9 + 7;
 
 int fac[N], ifac[N];
