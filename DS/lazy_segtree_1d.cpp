d0b014f0587c8a44c34713928645526cedeee06a
letuano5 <73065691+letuano5@users.noreply.github.com>
2022-06-08 12:53:44 +0700

    Update lazy_segtree_1d.cpp

--- DS/lazy_segtree_1d.cpp
+++ DS/lazy_segtree_1d.cpp
@@ -1,49 +1,49 @@
 struct SegTree {
   typedef int T;
-  T func (int a, int b) {
-    return a + b;
-  }
-
+  T func (const T& a, const T& b) { return a + b; }
+ 
   int n;
   T unit;
+  T unit_lazy;
   vector <T> st, lazy;
-
-  SegTree (int _n = 0, T _unit = 0) {
+ 
+  SegTree (int _n = 0, T _u = 0, T _ul = -1) {
     n = _n;
-    unit = _unit;
+    unit = _u;
+    unit_lazy = _ul;
     st.assign(4 * n + 5, unit);
-    lazy.assign(4 * n + 5, 0);
+    lazy.assign(4 * n + 5, unit_lazy);
   }
-
+ 
   void pushLazy (int id, int l, int r) {
-    if (lazy[id] == 0)
+    if (lazy[id] == unit_lazy)
       return;
-    int &add_val = lazy[id];
+    int& add_val = lazy[id];
     int mid = (l + r) >> 1;
-    st[id << 1]       += 1ll * (mid - l + 1) * add_val;
-    st[id << 1 | 1]   += 1ll * (r - mid) * add_val;
-    lazy[id << 1]     += add_val;
-    lazy[id << 1 | 1] += add_val;
-    add_val = 0;
+    st[id << 1]       = (mid - l + 1) * add_val;
+    st[id << 1 | 1]   = (r - mid) * add_val;
+    lazy[id << 1]     = add_val;
+    lazy[id << 1 | 1] = add_val;
+    add_val = unit_lazy;
   }
-
+ 
   void update (int id, int l, int r, int u, int v, T val) {
     if (v < l || r < u)
       return;
     if (u <= l && r <= v) {
-      st[id] += 1ll * (r - l + 1) * val;
-      lazy[id] += val;
+      st[id] = 1ll * (r - l + 1) * val;
+      lazy[id] = val;
       return;
     }
-
+ 
     int mid = (l + r) >> 1;
     pushLazy(id, l, r);
     update(id << 1, l, mid, u, v, val);
     update(id << 1 | 1, mid + 1, r, u, v, val);
-
+ 
     st[id] = func(st[id << 1], st[id << 1 | 1]);
   }
-
+ 
   T get (int id, int l, int r, int u, int v) {
     if (v < l || r < u) {
       return unit;
@@ -57,12 +57,12 @@ struct SegTree {
     T right = get(id << 1 | 1, mid + 1, r, u, v);
     return func(left, right);
   }
-
+ 
   void update (int L, int R, T val) {
     assert(L <= R && L >= 1 && R <= n);
     update(1, 1, n, L, R, val);
   }
-
+ 
   T get (int L, int R) {
     L = max(L, 1);
     R = min(R, this->n);
