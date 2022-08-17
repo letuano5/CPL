e8b16e03b64e3c13827ca2bb0a132d554856396c
letuano5 <73065691+letuano5@users.noreply.github.com>
2022-08-17 16:54:09 +0700

    Update pragma.cpp

--- pragma.cpp
+++ pragma.cpp
@@ -1,2 +1,2 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#pragma GCC optimize ("O3,unroll-loops,no-stack-protector")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
