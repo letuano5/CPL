492603153ece52d92f44bbb79cf5c19aa19abc3d
Le Tuan <73065691+letuano5@users.noreply.github.com>
2024-05-26 23:13:59 +0700

    Update hash.cpp

--- String/hash.cpp
+++ String/hash.cpp
@@ -66,3 +66,75 @@ struct Hash {
     h.emplace_back((__int128_t(h.back()) * BASE + c) % MOD);
   }
 };
+
+// multiple modulo
+struct Hash {
+  /*
+    static vector<int64_t> power = {1};
+    vector<int64_t> Hash::power;
+    do something with Hash::power
+  */
+  inline static vector<array<int64_t, 2>> power = {{1, 1}};
+  inline static int64_t BASE[2] = {-1, -1}, MOD[2] = {-1, -1};
+  vector<array<int64_t, 2>> h;
+  bool is_prime(int64_t x) {
+    for (int64_t i = 2; i * i <= x; i++) {
+      if (x % i == 0) {
+        return false;
+      }
+    }
+    return x > 1;
+  }
+  void init() {
+    for (int i = 0; i < 2; i++) {
+      if (BASE[i] == -1) {
+        do {
+          BASE[i] = rd(1e11, 1e12);
+        } while (!is_prime(BASE[i]));
+      }
+      if (MOD[i] == -1) {
+        do {
+          MOD[i] = rd(1e11, 1e12);
+        } while (!is_prime(MOD[i]));
+      }
+    }
+  }
+  Hash() {
+    init();
+  }
+  array<int64_t, 2> get(int l, int r) {
+    if (l > r) {
+      return {0, 0};
+    }
+    __int128_t ans[2];
+    for (int i = 0; i < 2; i++) {
+      ans[i] = (h[r][i] - __int128_t(h[l - 1][i]) * power[r - l + 1][i]) % MOD[i];
+      if (ans[i] < 0) ans[i] += MOD[i];
+    }
+    return {ans[0], ans[1]};
+  }
+  void push_back(char c) {
+    init();
+    if (power.empty()) power.push_back({1, 1});
+    if (h.empty()) h.push_back({0, 0});
+    array<__int128_t, 2> bk = {h.back()[0], h.back()[1]};
+    for (int i = 0; i < 2; i++) {
+      bk[i] = bk[i] * BASE[i] + c;
+      bk[i] %= MOD[i];
+    }
+    h.push_back(array<int64_t, 2>{int64_t(bk[0]), int64_t(bk[1])});
+    while (power.size() < h.size()) {
+      array<__int128_t, 2> bkk = {power.back()[0], power.back()[1]};
+      for (int i = 0; i < 2; i++) {
+        bkk[i] = bkk[i] * BASE[i] % MOD[i];
+      }
+      power.push_back(array<int64_t, 2>{int64_t(bkk[0]), int64_t(bkk[1])});
+    }
+  }
+  void change(array<int64_t, 2>& cur_val, int n, int pos, char old_c, char new_c) {
+    for (int i = 0; i < 2; i++) {
+      cur_val[i] = (cur_val[i] - __int128_t(power[n - pos][i]) * old_c % MOD[i] + MOD[i]) % MOD[i];
+      cur_val[i] = (cur_val[i] + __int128_t(power[n - pos][i]) * new_c % MOD[i] + MOD[i]) % MOD[i];
+    }
+  }
+};
