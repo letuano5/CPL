de29b8e3e1c54bf01a879df161f40d51a178fed4
lto5 <jssjsonsn@gmail.com>
2025-02-07 09:47:11 +0700

    faster matrix mult

--- math/matrix_exponentation.cpp
+++ math/matrix_exponentation.cpp
@@ -8,6 +8,8 @@ struct Matrix {
 
   int row() const { return a.size(); }
   int col() const { return a[0].size(); }
+  auto& operator[](int i) { return data[i]; }
+  const auto& operator[](int i) const { return data[i]; }
 
   friend ostream& operator<<(ostream& ost, const Matrix& x) {
     for (const auto& i : x.a) {
@@ -36,6 +38,37 @@ struct Matrix {
         }
     return ans;
   }
+
+/*
+  Faster trick by adamant: https://judge.yosupo.jp/submission/204842
+  Note: Matrix's data type must be uint64_t
+
+  const int mod = 998244353;
+  const uint64_t modmod = 1LL * mod * mod;
+
+  Matrix operator*(const Matrix& b) {
+    Matrix a = *this;
+    assert(a.col() == b.row());
+    Matrix c(a.row(), b.col());
+    for (int i = 0; i < a.row(); ++i)
+      for (int k = 0; k < a.col(); ++k) {
+        for (int j = 0; j < b.col(); ++j) {
+          c[i][j] += a[i][k] * b[k][j];
+        }
+        if (k % 8 == 0) {
+          for (int j = 0; j < b.col(); ++j) {
+            c[i][j] = min(c[i][j], c[i][j] - 8 * modmod);
+          }
+        }
+      }
+    for (int i = 0; i < a.row(); i++) {
+      for (int j = 0; j < b.col(); j++) {
+        c[i][j] %= mod;
+      }
+    }
+    return c;
+  }
+*/
 
   Matrix power(int64_t b) {
     Matrix a = *this, ans = identity(row());
