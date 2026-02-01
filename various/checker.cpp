88de862f9cb1ab17f2c348fa21965d30258ae5e2
Le Tuan <lto5@192.168.45.103>
2026-02-01 10:08:22 +0700

    test commit

--- various/checker.cpp
+++ various/checker.cpp
@@ -14,23 +14,20 @@ freopen("a.ans", "w", stdout);
 */
 
 mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
-long long get(long long l, long long h)
-{
+long long get(long long l, long long h) {
   return uniform_int_distribution<long long>(l, h)(rd);
 }
 
-int32_t main()
-{
-  for (int iTest = 1;; iTest++)
-  {
+int32_t main() {
+  for (int iTest = 1;; iTest++) {
     ofstream inp((NAME + ".inp").c_str());
 
-    //
+    // Gen test here, using inp instead of cout
 
     inp.close();
     system((NAME + ".exe").c_str());
     system((NAME + "_trau.exe").c_str());
-    if (system(("fc " + NAME + ".out " + NAME + ".ans").c_str()) != 0){
+    if (system(("fc " + NAME + ".out " + NAME + ".ans").c_str()) != 0) {
       cout << "Test " << iTest << ": WRONG!\n";
       return 0;
     }
